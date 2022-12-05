ece5a2e5a5be2fd3305f9040ad7d8bef89618a69
abdullah <abmunawer@gmail.com>
2022-12-06 01:03:08 +0500

    add delete feature

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import React, { useState, useEffect} from 'react'; 
+import List from './components/List';
+import Addlist from './components/Addlist'
+
+//https://www.youtube.com/watch?v=uTP3ffTx_Xs
+
+const getLocalItem = () =>{
+  let list = localStorage.getItem('lists');
+  if(list){
+    return JSON.parse(localStorage.getItem('lists'))
+  }else{
+    return[]
+  }
+}
+
+
+
 function App() {
+  
+   const [summary,setSummary]= useState(getLocalItem);
+  
+  
+  const updateListHandler = (newsummary) => {
+
+    setSummary([...summary, newsummary])
+ 
+  }
+
+useEffect(() => {
+  localStorage.setItem('lists', JSON.stringify(summary));
+},[summary]);
+
+const remove =(id)=>{
+  console.log(id)
+      const updatesummary = summary.filter((summary, index)=>{
+          return index !==id
+      });
+
+      setSummary(updatesummary);
+
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>List</h1>
+      <List className="check" onSave={updateListHandler} ></List>
+      <h1>Addlist</h1>
+      <Addlist summary={summary} removeList={remove}/>
+
     </div>
   );
 }
